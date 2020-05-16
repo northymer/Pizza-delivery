4411355ef6509d3d3bb35b751771aeffcbe01912
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-05-16 18:33:07 +0300

    fixing server for production

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(cors())
 
 app.use(express.json({extended: true}))
 
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/orders', require('./routes/orders.routes'))
+app.use('/api/auth', require('./src/server/routes/auth.routes'))
+app.use('/api/orders', require('./src/server/routes/orders.routes'))
 
 if (process.env.NODE_ENV === 'production') {
   console.log('cwd', process.cwd())
