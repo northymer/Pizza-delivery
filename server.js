ffc2a3d6f326d916a3b97409420ab6785831b652
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-05-16 19:28:03 +0300

    fixing server for production

--- server.js
+++ server.js
@@ -13,27 +13,26 @@ app.use(cors())
 
 app.use(express.json({extended: true}))
 
-app.use('/api/auth', require('./src/server/routes/auth.routes'))
-app.use('/api/orders', require('./src/server/routes/orders.routes'))
-
-if (process.env.NODE_ENV === 'production') {
-  console.log('cwd', process.cwd())
-  console.log('dirname', __dirname)
-  app.get('*', (req, res) => {
-    console.log(req)
-    res.sendFile(path.resolve(process.cwd(), 'build', 'index.html'))
-  })
-  app.use('/', express.static(path.join(process.cwd(), 'build')))
-  app.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), 'public', 'favicon.ico'))
-  })
-  app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')))
-
-}
-
 async function start() {
   try {
     console.log('started')
+    app.use('/api/auth', require('./src/server/routes/auth.routes'))
+    app.use('/api/orders', require('./src/server/routes/orders.routes'))
+
+    if (process.env.NODE_ENV === 'production') {
+      console.log('cwd', process.cwd())
+      console.log('dirname', __dirname)
+      app.get('*', (req, res) => {
+        console.log(req)
+        res.sendFile(path.resolve(process.cwd(), 'build', 'index.html'))
+      })
+      app.use('/', express.static(path.join(process.cwd(), 'build')))
+      app.get('/favicon.ico', (req, res) => {
+        res.sendFile(path.resolve(process.cwd(), 'public', 'favicon.ico'))
+      })
+      app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')))
+    }
+    console.log('use part')
     await mongoose.connect(config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
