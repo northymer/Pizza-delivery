7f11e6ffc3aca527ac96ccee65f5fc485a81399e
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-05-16 11:07:22 +0300

    fixing server for production

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors')
 
 const app = express()
@@ -12,20 +13,27 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/orders', require('./routes/orders.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '../..','build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../..', 'build', 'index.html'))
+  })
+}
+
 const PORT = config.get('port') || 5000
 
 async function start() {
-    try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        })
-        app.listen(PORT, () => console.log(`app has been started on port ${PORT}`))
-    } catch (e) {
-        console.log('Server Error')
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    app.listen(PORT, () => console.log(`app has been started on port ${PORT}`))
+  } catch (e) {
+    console.log('Server Error')
+    process.exit(1)
+  }
 }
 
 start()
