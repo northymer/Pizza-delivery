4d6e5b6fb9ec0d6c6f17faf6f3d9a40b64ae4858
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-05-27 16:29:00 +0300

    added favicon and fixed some lint errors

--- src/server/server.js
+++ src/server/server.js
@@ -11,13 +11,14 @@ const PORT = config.get('port') || 5000
 
 app.use(cors())
 
-app.use(express.json({extended: true}))
+app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/orders', require('./routes/orders.routes'))
 
 if (process.env.NODE_ENV === 'production') {
   try {
+    app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
     app.use('/', express.static(path.join(process.cwd(), 'build')))
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
